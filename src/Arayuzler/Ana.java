423b068d436317501488e277eb7ef7600a61ad27
YavuzKahraman <yavuzkhrmn@gmail.com>
2024-02-20 14:13:24 +0300

    .

--- src/Arayuzler/Ana.java
+++ src/Arayuzler/Ana.java
@@ -13,6 +13,17 @@ public class Ana {
 		
 		System.out.println("Kare alan: "+kareAlan+"\nKare cevre: "+kareCevre);
 		System.out.println("Daire alan: "+daireAlan+"\nDaire cevre: "+daireCevre);
+		System.out.println("********************");
+		ISekil sekil;
+		sekil = k;
+		double kAlan = sekil.alan(5);
+		double kCevre = sekil.cevre(5);
+		System.out.println("Kare alan: "+kAlan+"\nKare cevre: "+kCevre);
+		
+		sekil = d;
+		double dAlan = sekil.alan(5);
+		double dCevre = sekil.cevre(5);
+		System.out.println("Daire alan: "+dAlan+"\nDaire cevre: "+dCevre);
 
 	}
 
--- src/Arayuzler/ISekil.java
+++ src/Arayuzler/ISekil.java
@@ -1,10 +1,11 @@
 package Arayuzler;
 
 public interface ISekil {
-		// erişim belirleyicisi default ya da public olmak zorundadır.
-		// body kısmı yani parantezli kısmı olmaz. İmza şeklinde olmalıdır.
+	// erişim belirleyicisi default ya da public olmak zorundadır.
+	// body kısmı yani parantezli kısmı olmaz. İmza şeklinde olmalıdır.
+	// arayüzlerin değişken tanımlama özellikler bulunmaz. Yani
+	// int sayi; şeklinde tanımlama yapılamaz.	
+	
 	public double alan(int a);
 	public double cevre(int a);
-	
-
 }
